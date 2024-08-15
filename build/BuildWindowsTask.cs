2d3e1120b328870497e883cf269a40a1a5253f79
Dean Ellis <dellis1972@googlemail.com>
2024-08-15 12:41:40 +0100

    Fix windows

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -14,8 +14,9 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         // Path relative to the buildWorkingDir
         var cmakeListsPath = System.IO.Path.Combine("..", "crunch", "CMakeLists.txt");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DBUILD_CRUNCH=ON -DBUILD_SHARED_LIBCRN=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_EXAMPLES=OFF {cmakeListsPath}" });
-        context.ReplaceTextInFiles("crunch_build/crunch.vcxproj", "MultiThreadedDLL", "MultiThreaded");
-        //context.ReplaceTextInFiles("crunch_build/crnlib/crn-obj.vcxproj",  "MultiThreadedDLL", "MultiThreaded");
+        context.ReplaceTextInFiles("crunch_build/_crunch/crunch.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        context.ReplaceTextInFiles("crunch_build/crnlib/crn-obj.vcxproj",  "MultiThreadedDLL", "MultiThreaded");
+        context.ReplaceTextInFiles("crunch_build/crnlib/crn.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
         var files = Directory.GetFiles(System.IO.Path.Combine (buildWorkingDir, "Release"), "crunch.exe", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/crunch.exe");
