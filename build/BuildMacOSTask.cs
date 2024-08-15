03e8d576edca0702056415554cea107e0f741005
Dean Ellis <dellis1972@googlemail.com>
2024-08-15 10:24:32 +0100

    Do not build a bunch of stuff

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -13,7 +13,7 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
         Directory.CreateDirectory(buildWorkingDir);
         // Path relative to the buildWorkingDir
         var cmakeListsPath = System.IO.Path.Combine("..", "crunch", "CMakeLists.txt");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" {cmakeListsPath}" });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" -DCMAKE_BUILD_TYPE=Release -DBUILD_CRUNCH=ON -DBUILD_SHARED_LIBCRN=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_EXAMPLES=OFF {cmakeListsPath}" });
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
         var files = Directory.GetFiles(buildWorkingDir, "crunch", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/crunch");
--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -13,7 +13,7 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         Directory.CreateDirectory(buildWorkingDir);
         // Path relative to the buildWorkingDir
         var cmakeListsPath = System.IO.Path.Combine("..", "crunch", "CMakeLists.txt");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = cmakeListsPath });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DBUILD_CRUNCH=ON -DBUILD_SHARED_LIBCRN=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_EXAMPLES=OFF {cmakeListsPath}" });
         context.ReplaceTextInFiles("crunch_build/crunch.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         //context.ReplaceTextInFiles("crunch_build/crnlib/crn-obj.vcxproj",  "MultiThreadedDLL", "MultiThreaded");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
